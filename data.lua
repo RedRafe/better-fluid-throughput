c5b5e69e2783ab0d00586ccb43e4133200f28e55
RedRafe <gabri97.gt+redrafe@gmail.com>
2024-05-09 23:54:15 +0200

    release v0.1.6

--- data.lua
+++ data.lua
@@ -3,12 +3,16 @@ BFT_blacklist = {
     ['se-liquid-rocket-fuel'] = true,
     ['se-ion-stream'] = true,
     ['se-antimatter-stream'] = true,
+    ['ll-oxygen'] = true,
   },
   ['storage-tank'] = {
     ['se-rocket-launch-pad-tank'] = true,
     ['se-spaceship-rocket-booster-tank'] = true,
     ['se-spaceship-ion-booster-tank'] = true, 
     ['se-spaceship-antimatter-booster-tank'] = true,
+    ['ll-oxygen-diffuser-fluidbox'] = true,
+    ['ll-arc-furnace-storage-tank'] = true,
+
   },
   ['generator'] = {
     ['se-fluid-burner-generator'] = true,
